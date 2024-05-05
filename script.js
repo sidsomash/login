ce564cb1a7a608862be1b88603521ad6ad767264
sidsomashekar <sidsomash@gmail.com>
2024-05-05 18:15:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-let loginEmailInput = document.getElementById("loginEmail");
-let loginPasswordInput = document.getElementById("loginPassword");
-let loginBtn = document.getElementById("loginBtn");
+var database = [
+    {
+        username: "sidsomash",
+        password: "newsite123"
+    },
+    {
+        username: "admin",
+        password: "newadmin123"
+    }
+]
 
-let dataBase = {
-    'sidsomash' : 'welcome'
-}
+function getInfo() {
+    var username = document.getElementById(username).value
+    var password = document.getElementById(password).value
 
-function isUser() {
-    if
+    for (var i = 0; i < database.length; i++) {
+        if (username[i] == database[i].username && password[i] == database[i].password) {
+            console.log("login successful!");
+            window.location.href = 'https://www.linkedin.com'
+        }
+        else {
+            console.log("login failed!")
+            if (username[i] != database[i].username && password[i] != database[i].password) {
+                console.log("incorrect username and password!");
+            }
+            else if (username[i] != database[i].username) {
+                console.log("incorrect username!");
+            }
+            else if (password[i] != database[i].password) {
+                console.log("incorrect password!");
+            }
+        }
+    }
 }
