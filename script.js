d2065e2445af981114301ca90f4c4600f9383177
sidsomash <sidsomash@gmail.com>
2024-05-05 13:10:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function getInfo() {
-    
+let loginEmailInput = document.getElementById("loginEmail");
+let loginPasswordInput = document.getElementById("loginPassword");
+let loginBtn = document.getElementById("loginBtn");
+
+let dataBase = {
+    'sidsomash' : 'welcome'
+}
+
+function isUser() {
+    if
 }
